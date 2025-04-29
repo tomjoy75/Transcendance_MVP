d79607345a4927ed3f5ccf7112cfee2dac0ef16b
tomjoy75 <tomjoy75@gmail.com>
2025-04-30 00:44:33 +0200

    exercise on server in node.js

--- exos/dyma/serveur/app.js
+++ exos/dyma/serveur/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+// Formule simple
 // const server = http.createServer((req, res) => {
 // 	console.log('got a request');
 // 	res.end();
@@ -8,8 +9,12 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-	console.log('got a request'); // Fonction de callback pour lire la requete et ecrire la reponse
-	res.end();
-})
+//	console.log(req);
+//	console.log('got a request'); // Fonction de callback pour lire la requete et ecrire la reponse
+	res.writeHead(200, {
+		'content-type':'text/plain'
+	})
+	res.end('got a response');
+});
 
-server.listen(8081);
+server.listen(8080);
